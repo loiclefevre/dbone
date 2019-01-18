bcba4df96a0e875eadb23bb0e80eef5bcf2e6ccd
Loïc <loic.lefevre@gmail.com>
2019-01-18 15:38:57 +0100

    Update cci_ddl.sql

--- cci/src/cci_ddl.sql
+++ cci/src/cci_ddl.sql
@@ -14,11 +14,11 @@ create table identity_domains (
     administrator_name varchar2(128) not null,
     administrator_password varchar2(128) not null,
     creation_date varchar2(64),
-	  client_id varchar2(64),
-	  client_secret varchar2(64),
+    client_id varchar2(64),
+    client_secret varchar2(64),
     administrator_ocid varchar2(256),
     administrator_key_fingerprint varchar2(256),
-    administrator_prvate_key varchar2(4000),
+    administrator_private_key varchar2(4000),
     default_key_fingerprint varchar2(256),
     default_public_key_pem varchar2(4000),
     oci_idp_ocid varchar2(256)
@@ -31,5 +31,23 @@ comment on column identity_domains.region is 'Identity domain region (example: u
 comment on column identity_domains.description is 'Identity domain description.';
 comment on column identity_domains.idcs_identifier is 'Identity domain IDCS identifier (example: idcs-xxxxxxxxxxxxxxxxxxxxxxx).';
 comment on column identity_domains.cloud_account is 'Identity domain Cloud Account (example: cacct-yyyyyyyyyyyyyyyyyyyyyyyyyy).';
+comment on column identity_domains.tenant_ocid is 'Identity domain Tenant OCID (example: ocid1.tenancy.oc1..abcdefghijklmnop...).';
+comment on column identity_domains.administrator_name is 'Identity domain administrator name. This is a valid Oracle Cloud user used for REST API Basic authentication to Oracle Cloud Metering REST API.';
+comment on column identity_domains.administrator_password is 'Identity domain administrator password.';
+comment on column identity_domains.creation_date is 'Identity domain creation date, filled automatically by CCI.';
+comment on column identity_domains.client_id is 'IDCS application OAuth2 Client Id used by CCI to perform IDCS administration tasks.';
+comment on column identity_domains.client_secret is 'IDCS application OAuth2 Client Secret used by CCI to perform IDCS administration tasks.';
+comment on column identity_domains.administrator_ocid is 'OCI non federated User OCID belonging to the Administrators group.';
+comment on column identity_domains.administrator_ocid is 'OCI non federated User OCID belonging to the Administrators group.';
+comment on column identity_domains.administrator_key_fingerprint is 'One of the OCI non federated User''s key fingerprint.';
+comment on column identity_domains.administrator_private_key is 'The associated OCI non federated User''s private key (RSA format).';
+comment on column identity_domains.default_key_fingerprint is 'Key fingerprint of the default API Key uploaded when creating federated Users.';
+comment on column identity_domains.default_public_key_pem is 'Default public API Key uploaded when creating federated Users (PEM format).';
+comment on column identity_domains.oci_idp_ocid is 'OCI Identity Provier OCID (OCID of the IDCS Identity Provider).';
+
+
+
+
+
 
 
